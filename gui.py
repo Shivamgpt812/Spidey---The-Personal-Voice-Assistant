b0578512d1ba5d76eb3312ecdd9f603aceb4a2c0
Shivam Gupta <144247614+Shivamgpt812@users.noreply.github.com>
2024-05-30 18:07:09 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -20,7 +20,7 @@ def ask():
     bot_val = action.Action(ask_val)
     text.insert(END, "Me --> "+ask_val+"\n") 
     if bot_val != None:
-       text.insert(END, "Bot <-- "+ str(bot_val)+"\n")
+       text.insert(END, "Spidey <-- "+ str(bot_val)+"\n")
     if bot_val == "ok sir":
         root.destroy()
 
@@ -30,20 +30,20 @@ def delete_text():
 
 root = Tk()
 root.geometry("550x675")
-root.title("AI Assistant")
+root.title("Spidey - The Personl Voice Assistant")
 root.resizable(False,False)
-root.config(bg="#6F8FAF")
+root.config(bg="#ffffff")
 
   
 
 
 # Main Frame
-Main_frame = LabelFrame(root , padx=100 ,  pady=7 , borderwidth=3 ,  relief="raised")
-Main_frame.config(bg="#6F8FAF")
+Main_frame = LabelFrame(root , padx=100 ,  pady=7 , borderwidth=0 ,  relief="raised")
+Main_frame.config(bg="#ffffff")
 Main_frame.grid(row = 0 ,  column= 1 ,  padx= 55 ,  pady =  10)
 
 # Text Lable 
-Text_lable = Label(Main_frame, text = "Spidey" , font=("comic Sans ms" ,  14 , "bold" ) , bg = "#356696")
+Text_lable = Label(Main_frame, text = "Spidey" , font=("comic Sans ms" ,  14 , "bold" ) , bg = "#ffffff")
 Text_lable.grid(row=0 ,  column=0 , padx=20 , pady= 10)
 
 
@@ -56,7 +56,7 @@ Image_Lable.grid(row = 1 ,  column=0 , pady=20)
 
 # Add a text widget
 
-text=Text(root , font= ('Courier 10 bold') , bg = "#356696")
+text=Text(root , font= ('Courier 10 bold') , bg = "#ffffff", borderwidth=1, relief=SOLID)
 text.grid(row = 2,  column= 0)
 text.place(x= 100, y= 375, width= 375, height= 100) 
 
@@ -64,20 +64,20 @@ text.place(x= 100, y= 375, width= 375, height= 100)
 
 
 # Add a entry widget
-entry1 = Entry(root, justify = CENTER)
+entry1 = Entry(root, justify = CENTER,  borderwidth=1 , relief=SOLID )
 entry1.place(x=100 , y = 500 , width= 350, height= 30)
 
 
 
 # Add a text button1
-button1 =  Button(root,  text="ASK" , bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=ask)
+button1 =  Button(root,  text="ASK" , bg="#ffffff" , pady=16 ,  padx=40,  borderwidth=1 , relief=SOLID ,  command=ask)
 button1.place(x= 70, y= 575)
 
 # Add a text button2
-button2 =  Button(root,  text="Send" , bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,  command=User_send)
+button2 =  Button(root,  text="Send" , bg="#ffffff" , pady=16 ,  padx=40,  borderwidth=1 , relief=SOLID ,  command=User_send)
 button2.place(x= 400, y= 575)
 
 # Add a text button3
-button3 = Button(root, text="Delete", bg="#356696" , pady=16 ,  padx=40,  borderwidth=3 , relief=SOLID ,command=delete_text)
+button3 = Button(root, text="Delete", bg="#ffffff" , pady=16 ,  padx=40,  borderwidth=1 , relief=SOLID ,command=delete_text)
 button3.place(x= 225, y= 575)
 root.mainloop()
